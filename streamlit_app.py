c92085f7e78b18514efb05ee92c81fd89bde846a
Hadiya Murad Hadi <164493973+Hadiya-Murad-Hadi@users.noreply.github.com>
2025-05-28 13:12:30 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🤖 Machine Learning App')
 st.info('This app builds models hehe ')
 
 with st.expander('Data'):
-  st.write(**Raw Data**)
+  st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
